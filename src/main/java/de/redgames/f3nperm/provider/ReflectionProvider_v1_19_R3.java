b088eed3891e748b42632eec8487e98cebfdda9b
Rodney_Mc_Kay <36546810+RodneyMKay@users.noreply.github.com>
2023-04-20 00:02:17 +0200

    Fix the mapping for the networkManager in 1.19.4

--- src/main/java/de/redgames/f3nperm/provider/ReflectionProvider_v1_19_R3.java
+++ src/main/java/de/redgames/f3nperm/provider/ReflectionProvider_v1_19_R3.java
@@ -4,7 +4,13 @@ import de.redgames.f3nperm.reflection.ReflectionException;
 import de.redgames.f3nperm.reflection.Reflections;
 
 public class ReflectionProvider_v1_19_R3 extends ReflectionProvider_v1_19_R1 {
+    @Override
     public Object getPlayerConnection(Object entityPlayer) throws ReflectionException {
         return Reflections.get(entityPlayer, "b");
+    }
+
+    @Override
+    public Object getNetworkManager(Object playerConnection) throws ReflectionException {
+        return Reflections.get(playerConnection, "h");
     }
 }
