00e1ae82cb194d8df35634e8738c4ae01259f061
BlockyTheDev <86119630+BlockyTheDev@users.noreply.github.com>
2023-04-19 21:49:20 +0200

    Correct my mistake and give class a better name

--- src/main/java/de/redgames/f3nperm/F3NPermPlugin.java
+++ src/main/java/de/redgames/f3nperm/F3NPermPlugin.java
@@ -18,18 +18,18 @@ public final class F3NPermPlugin extends JavaPlugin implements Listener {
     };
 
     private Provider provider;
-    private ServerVersion serverVersion;
+    private NMSVersion nmsVersion;
     private Settings settings;
     private List<Hook> registeredHooks;
 
     @Override
     public void onLoad() {
-        serverVersion = ServerVersion.fromBukkitVersion();
+        nmsVersion = NMSVersion.fromBukkitVersion();
 
-        if (serverVersion == null) {
+        if (nmsVersion == null) {
             getLogger().warning("Could not read server version, proceed with caution!");
         } else {
-            getLogger().info("Server version " + serverVersion + " detected");
+            getLogger().info("Server version " + nmsVersion + " detected");
         }
 
         loadSettings();
@@ -117,23 +117,23 @@ public final class F3NPermPlugin extends JavaPlugin implements Listener {
             }
         }
 
-        if (serverVersion == null) {
+        if (nmsVersion == null) {
             throw new ProviderException("Server version cannot be detected and ProtocolLib is disabled!");
         }
 
-        if (serverVersion.isLowerThan(ServerVersion.v_1_17)) {
+        if (nmsVersion.isLowerThan(NMSVersion.v1_17_R1)) {
             return new ReflectionProvider_1_9();
         }
 
-        if (serverVersion.isLowerThan(ServerVersion.v_1_18)) {
+        if (nmsVersion.isLowerThan(NMSVersion.v1_18_R1)) {
             return new ReflectionProvider_1_17();
         }
 
-        if (serverVersion.isLowerThan(ServerVersion.v_1_18_2)) {
+        if (nmsVersion.isLowerThan(NMSVersion.v1_18_R2)) {
             return new ReflectionProvider_1_18();
         }
 
-        if (serverVersion.isLowerThan(ServerVersion.v_1_19_1)) {
+        if (nmsVersion.isLowerThan(NMSVersion.v1_19_R1)) {
             return new ReflectionProvider_1_18_2();
         }
 
--- src/main/java/de/redgames/f3nperm/NMSVersion.java
+++ src/main/java/de/redgames/f3nperm/NMSVersion.java
@@ -5,15 +5,15 @@ import org.bukkit.Bukkit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class ServerVersion {
-    public static final ServerVersion v_1_17 = new ServerVersion(1, 17, 0);
-    public static final ServerVersion v_1_18 = new ServerVersion(1, 18, 0);
-    public static final ServerVersion v_1_18_2 = new ServerVersion(1, 18, 2);
-    public static final ServerVersion v_1_19_1 = new ServerVersion(1, 19, 1);
+public final class NMSVersion {
+    public static final NMSVersion v1_17_R1 = new NMSVersion(1, 17, 1);
+    public static final NMSVersion v1_18_R1 = new NMSVersion(1, 18, 1);
+    public static final NMSVersion v1_18_R2 = new NMSVersion(1, 18, 2);
+    public static final NMSVersion v1_19_R1 = new NMSVersion(1, 19, 1);
 
     private static final Pattern PACKAGE_PATTERN = Pattern.compile("v(\\d+)_(\\d+)_R(\\d+)");
 
-    public static ServerVersion fromBukkitVersion() {
+    public static NMSVersion fromBukkitVersion() {
         String packageName;
 
         try {
@@ -32,20 +32,20 @@ public final class ServerVersion {
         int minor = Integer.parseInt(matcher.group(2));
         int patch = Integer.parseInt(matcher.group(3));
 
-        return new ServerVersion(major, minor, patch);
+        return new NMSVersion(major, minor, patch);
     }
 
     private final int major;
     private final int minor;
     private final int patch;
 
-    public ServerVersion(int major, int minor, int patch) {
+    public NMSVersion(int major, int minor, int patch) {
         this.major = major;
         this.minor = minor;
         this.patch = patch;
     }
 
-    public boolean isLowerThan(ServerVersion version) {
+    public boolean isLowerThan(NMSVersion version) {
         if (getMajor() < version.getMajor()) return true;
         if (getMajor() > version.getMajor()) return false;
         if (getMinor() < version.getMinor()) return true;
